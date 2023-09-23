d9dbb4545bb5b1be11ca41dec6b0bb5c2963f122
liucharlie213 <liu.charlie213@gmail.com>
2023-09-23 14:32:47 -0400

    Set up React App scaffolding

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Landing from './components/landing/Landing'
+import Chat from './components/chat/Chat'
+const App = () => {
+  return (
+        <p>Hello WOrld wejfbowien</p>
+  )
+}
+
+export default App
